807a8d83dad4ead2f0328923deb04186a49fa56c
disintegrating8 <disintegrating8@pm.me>
2025-09-17 22:51:02 +0900

    Update darwin.sh

--- darwin.sh
+++ darwin.sh
@@ -138,58 +138,58 @@ removeAnimations() {
     
     # Reduce motion in Accessibility settings (most effective)
     printf "%b\n" "${CYAN}Setting reduce motion preference...${RC}"
-    sudo defaults write com.apple.universalaccess reduceMotion -bool true
+    defaults write com.apple.universalaccess reduceMotion -bool true
     
     # Disable window animations
     printf "%b\n" "${CYAN}Disabling window animations...${RC}"
-    sudo defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+    defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
     
     # Speed up window resize animations
     printf "%b\n" "${CYAN}Speeding up window resize animations...${RC}"
-    sudo defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+    defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
     
     # Disable smooth scrolling
     printf "%b\n" "${CYAN}Disabling smooth scrolling...${RC}"
-    sudo defaults write NSGlobalDomain NSScrollAnimationEnabled -bool false
+    defaults write NSGlobalDomain NSScrollAnimationEnabled -bool false
     
     # Disable animation when opening and closing windows
     printf "%b\n" "${CYAN}Disabling window open/close animations...${RC}"
-    sudo defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+    defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
     
     # Disable animation when opening a Quick Look window
     printf "%b\n" "${CYAN}Disabling Quick Look animations...${RC}"
-    sudo defaults write -g QLPanelAnimationDuration -float 0
+    defaults write -g QLPanelAnimationDuration -float 0
     
     # Disable animation when opening the Info window in Finder
     printf "%b\n" "${CYAN}Disabling Finder Info window animations...${RC}"
-    sudo defaults write com.apple.finder DisableAllAnimations -bool true
+    defaults write com.apple.finder DisableAllAnimations -bool true
     
     # Speed up Mission Control animations
     printf "%b\n" "${CYAN}Speeding up Mission Control animations...${RC}"
-    sudo defaults write com.apple.dock expose-animation-duration -float 0.1
-    sudo defaults write com.apple.dock expose-group-apps -bool true
+    defaults write com.apple.dock expose-animation-duration -float 0.1
+    defaults write com.apple.dock expose-group-apps -bool true
 
     # Speed up Launchpad animations
     printf "%b\n" "${CYAN}Speeding up Launchpad animations...${RC}"
-    sudo defaults write com.apple.dock springboard-show-duration -float 0.1
-    sudo defaults write com.apple.dock springboard-hide-duration -float 0.1
+    defaults write com.apple.dock springboard-show-duration -float 0.1
+    defaults write com.apple.dock springboard-hide-duration -float 0.1
     
     # Disable dock hiding animation
     printf "%b\n" "${CYAN}Disabling dock hiding animations...${RC}"
-    sudo defaults write com.apple.dock autohide-time-modifier -float 0
-    sudo defaults write com.apple.dock autohide-delay -float 0
+    defaults write com.apple.dock autohide-time-modifier -float 0
+    defaults write com.apple.dock autohide-delay -float 0
     
     # Disable animations in Mail.app
     printf "%b\n" "${CYAN}Disabling Mail animations...${RC}"
-    sudo defaults write com.apple.mail DisableReplyAnimations -bool true
-    sudo defaults write com.apple.mail DisableSendAnimations -bool true
+    defaults write com.apple.mail DisableReplyAnimations -bool true
+    defaults write com.apple.mail DisableSendAnimations -bool true
     
     # Disable zoom animation when focusing on text input fields
     printf "%b\n" "${CYAN}Disabling text field zoom animations...${RC}"
-    sudo defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
+    defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
     
     printf "%b\n" "${GREEN}Motion and animations have been reduced.${RC}"
-    sudo killall Dock
+    killall Dock
     printf "%b\n" "${YELLOW}Dock Restarted.${RC}"
 }
 
@@ -198,29 +198,29 @@ fixfinder () {
 
     # Set the default Finder view to list view
     printf "%b\n" "${CYAN}Setting default Finder view to list view...${RC}"
-    sudo defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+    defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
     
     # Configure list view settings for all folders
     printf "%b\n" "${CYAN}Configuring list view settings for all folders...${RC}"
     # Set default list view settings for new folders
-    sudo defaults write com.apple.finder FK_StandardViewSettings -dict-add ListViewSettings '{ "columns" = ( { "ascending" = 1; "identifier" = "name"; "visible" = 1; "width" = 300; }, { "ascending" = 0; "identifier" = "dateModified"; "visible" = 1; "width" = 181; }, { "ascending" = 0; "identifier" = "size"; "visible" = 1; "width" = 97; } ); "iconSize" = 16; "showIconPreview" = 0; "sortColumn" = "name"; "textSize" = 13; "useRelativeDates" = 1; }'
+    defaults write com.apple.finder FK_StandardViewSettings -dict-add ListViewSettings '{ "columns" = ( { "ascending" = 1; "identifier" = "name"; "visible" = 1; "width" = 300; }, { "ascending" = 0; "identifier" = "dateModified"; "visible" = 1; "width" = 181; }, { "ascending" = 0; "identifier" = "size"; "visible" = 1; "width" = 97; } ); "iconSize" = 16; "showIconPreview" = 0; "sortColumn" = "name"; "textSize" = 13; "useRelativeDates" = 1; }'
     
     # Clear existing folder view settings to force use of default settings
     printf "%b\n" "${CYAN}Clearing existing folder view settings...${RC}"
-    sudo defaults delete com.apple.finder FXInfoPanesExpanded 2>/dev/null || true
-    sudo defaults delete com.apple.finder FXDesktopVolumePositions 2>/dev/null || true
+    defaults delete com.apple.finder FXInfoPanesExpanded 2>/dev/null || true
+    defaults delete com.apple.finder FXDesktopVolumePositions 2>/dev/null || true
     
     # Set list view for all view types
     printf "%b\n" "${CYAN}Setting list view for all folder types...${RC}"
-    sudo defaults write com.apple.finder FK_StandardViewSettings -dict-add ExtendedListViewSettings '{ "columns" = ( { "ascending" = 1; "identifier" = "name"; "visible" = 1; "width" = 300; }, { "ascending" = 0; "identifier" = "dateModified"; "visible" = 1; "width" = 181; }, { "ascending" = 0; "identifier" = "size"; "visible" = 1; "width" = 97; } ); "iconSize" = 16; "showIconPreview" = 0; "sortColumn" = "name"; "textSize" = 13; "useRelativeDates" = 1; }'
+    defaults write com.apple.finder FK_StandardViewSettings -dict-add ExtendedListViewSettings '{ "columns" = ( { "ascending" = 1; "identifier" = "name"; "visible" = 1; "width" = 300; }, { "ascending" = 0; "identifier" = "dateModified"; "visible" = 1; "width" = 181; }, { "ascending" = 0; "identifier" = "size"; "visible" = 1; "width" = 97; } ); "iconSize" = 16; "showIconPreview" = 0; "sortColumn" = "name"; "textSize" = 13; "useRelativeDates" = 1; }'
     
     # Sets default search scope to the current folder
     printf "%b\n" "${CYAN}Setting default search scope to the current folder...${RC}"
-    sudo defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
     # Remove trash items older than 30 days
     printf "%b\n" "${CYAN}Removing trash items older than 30 days...${RC}"
-    sudo defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
+    defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
 
     # Remove .DS_Store files to reset folder view settings
     printf "%b\n" "${CYAN}Removing .DS_Store files to reset folder view settings...${RC}"
@@ -228,29 +228,29 @@ fixfinder () {
 
     # Show all filename extensions
     printf "%b\n" "${CYAN}Showing all filename extensions in Finder...${RC}"
-    sudo defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
     # Set the sidebar icon size to small
     printf "%b\n" "${CYAN}Setting sidebar icon size to small...${RC}"
-    sudo defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+    defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
 
     # Show status bar in Finder
     printf "%b\n" "${CYAN}Showing status bar in Finder...${RC}"
-    sudo defaults write com.apple.finder ShowStatusBar -bool true
+    defaults write com.apple.finder ShowStatusBar -bool true
 
     # Show path bar in Finder
     printf "%b\n" "${CYAN}Showing path bar in Finder...${RC}"
-    sudo defaults write com.apple.finder ShowPathbar -bool true
+    defaults write com.apple.finder ShowPathbar -bool true
 
     # Clean up Finder's sidebar
     printf "%b\n" "${CYAN}Cleaning up Finder's sidebar...${RC}"
-    sudo defaults write com.apple.finder SidebarDevicesSectionDisclosedState -bool true
-    sudo defaults write com.apple.finder SidebarPlacesSectionDisclosedState -bool true
-    sudo defaults write com.apple.finder SidebarShowingiCloudDesktop -bool false
+    defaults write com.apple.finder SidebarDevicesSectionDisclosedState -bool true
+    defaults write com.apple.finder SidebarPlacesSectionDisclosedState -bool true
+    defaults write com.apple.finder SidebarShowingiCloudDesktop -bool false
 
     # Restart Finder to apply changes
     printf "%b\n" "${GREEN}Finder has been restarted and settings have been applied.${RC}"
-    sudo killall Finder
+    killall Finder
 }
 
 my_defaults(){
@@ -293,9 +293,9 @@ main() {
 	    stow_dotfiles
 	    ;;
 	5) 
-	    removeAnimations
-	    fixfinder
-	    my_defaults
+	    #removeAnimations
+	    #fixfinder
+	    #my_defaults
 	    install_my_apps
 	    ;;
 	6)
@@ -311,9 +311,9 @@ main() {
 	    install_nvim
 	    install_yabai
 	    stow_dotfiles
-	    removeAnimations
-	    fixfinder
-	    my_defaults
+	    #removeAnimations
+	    #fixfinder
+	    #my_defaults
 	    install_my_apps
 	    ;;
 	*) printf "%b\n" "${RED}Invalid choice.${RC}" && exit 1 ;;
